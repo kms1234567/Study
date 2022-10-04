f57dd7934c2bb5369fc9dc0ea60b03b576468fb2
kms1234567 <rudwls7271@naver.com>
2022-10-04 23:31:39 +0900

    221014

---
+++ LeetCode/70. Climbing Stairs.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [1, 1] + [0] * (n-1)
+        
+        for i in range(2, n+1):
+            dp[i] = dp[i-2] + dp[i-1]
+        
+        return dp[-1]
