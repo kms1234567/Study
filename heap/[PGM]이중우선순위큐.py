9e9575bb5bdb271f5ec1e8032ec48baa7953e918
kms1234567 <rudwls7271@naver.com>
2022-10-24 23:13:59 +0900

    221024

--- heap/[PGM]이중우선순위큐.py
+++ heap/[PGM]이중우선순위큐.py
@@ -28,3 +28,67 @@ def solution(operations):
         return [0, 0]
     else:
         return [-heappop(hq_max), heappop(hq_min)]
+
+# 22.10.24
+from heapq import heapify, heappop, heappush
+
+def solution(operations):
+    answer = []
+
+    min_heap = []
+    max_heap = []
+    data_set = set()
+    
+    for operation in operations:
+        op, num = operation.split(' ')
+        num = int(num)
+        if op == 'I':
+            heappush(min_heap, num)
+            heappush(max_heap, -num)
+            data_set.add(num)          
+        else:
+            while data_set:
+                if num > 0:
+                    data = -heappop(max_heap)
+                else:
+                    data = heappop(min_heap)
+                if data in data_set:
+                    data_set.remove(data)
+                    break
+                
+    if not data_set:
+        return [0, 0]
+    else:
+        data = None
+        while data not in data_set:
+            data = -heappop(max_heap)
+        answer.append(data)
+        
+        data = None
+        while data not in data_set:
+            data = heappop(min_heap)
+        answer.append(data)
+
+    return answer
+
+import heapq
+
+def solution(operations):
+    heap = []
+
+    for operation in operations:
+        operator, operand = operation.split(' ')
+        operand = int(operand)
+
+        if operator == 'I':
+            heapq.heappush(heap, operand)
+        elif heap:
+            if operand < 0:
+                heapq.heappop(heap)
+            else:
+                heap.remove(max(heap))
+
+    if not heap:
+        return [0, 0]
+
+    return [max(heap), heap[0]]
